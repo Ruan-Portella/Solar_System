0a61b5ede5cc23b673461feb74badb2c21f6382a
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-01 16:12:39 -0300

    Feat: Resolvendo Exercicio 06

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        <Title headline="Planetas" />
-      </div>
+      <>
+        <div data-testid="solar-system">
+          <Title headline="Planetas" />
+        </div>
+        {Planets
+          .map((planet) => (<PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />))}
+      </>
     );
   }
 }
