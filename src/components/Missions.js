a6b99ac1ead0fe531045dc6d722f36fd42282b8c
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-01 16:48:41 -0300

    Feat: Adicionando exercicio 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
@@ -7,6 +9,14 @@ class Missions extends React.Component {
       <>
         <div data-testid="missions" />
         <Title headline="Missões" />
+        {missions
+          .map((mission) => (<MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />))}
       </>
     );
   }
