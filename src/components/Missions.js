374902fb2ecf95aaa33c4b34361b401d2fa21134
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-01 16:26:15 -0300

    Feat: Resolvendo Exercicio 08

---
+++ src/components/Missions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from './Title';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <>
+        <div data-testid="missions" />
+        <Title headline="Missões" />
+      </>
+    );
+  }
+}
+
+export default Missions;
