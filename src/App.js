7ba4199d5c163e41d985ceb5c9e3efaabf460fe9
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-01 15:32:45 -0300

    Feat: Adicionando compontentes ao App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solasr</p>);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
 
